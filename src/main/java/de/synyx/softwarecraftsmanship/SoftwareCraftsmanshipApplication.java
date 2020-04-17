c3fc781530a9ce0f87a4322d87bea0a36e9cc189
Sven Müller <mueller@synyx.de>
2020-04-17 14:34:48 +0200

    initial commit, spring initializer

---
+++ src/main/java/de/synyx/softwarecraftsmanship/SoftwareCraftsmanshipApplication.java
@@ -0,0 +1,13 @@
+package de.synyx.softwarecraftsmanship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareCraftsmanshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareCraftsmanshipApplication.class, args);
+    }
+
+}
